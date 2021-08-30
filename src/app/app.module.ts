3c16d2718766a8a8fae5e676ce042d4421f81747
Kauã Nunes <kauanunnes@hotmail.com>
2021-08-30 18:16:49 -0300

    refatorando form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoItemsComponent } from './views/to-do-items/to-do-items.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './views/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoItemsComponent,
+    FormComponent,
 
   ],
   imports: [
