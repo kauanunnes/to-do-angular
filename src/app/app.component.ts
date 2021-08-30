eb68cf7aa7c1a61d2e88062c788a826ec142d92a
Kauã Nunes <kauanunnes@hotmail.com>
2021-08-30 18:36:18 -0300

    service finalizada!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as uniqid from 'uniqid';
 import { Item } from './models/Item';
+import { TasksService } from './services/tasks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
   title = 'To-do List';
-  toDoList: Item[] = [];
+  TaskService: TasksService = new TasksService()
+  toDoList!: Item[];
   data = window.localStorage.getItem('todolist');
 
   ngOnInit() {
-    if (!this.data) {
-      window.localStorage.setItem('todolist', '[]');
-      this.toDoList = [];
-    } else {
-      this.toDoList = JSON.parse(this.data).map((task: Item) => {
-        return task;
-      });
-    }
+    this.toDoList = this.TaskService.getTasks()
   }
+
 
   addNewTask(newTask: string) {
     this.toDoList.push({
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../models/Item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
+  private tasks!: Item[]
+  constructor() {
+  }
 
-  constructor() { }
+  getTasks():Item[] {
+    const data = window.localStorage.getItem('todolist');
+    if (!data) {
+      window.localStorage.setItem('todolist', '[]');
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(data).map((task: Item) => {
+        return task;
+      });
+    }
+    return this.tasks
+  }
 }
